bb54ac2c36752b49cd52ce8636c8b9c07bf58593
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-01-30 17:01:21 +0530

    added signup

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 import ColorModeToggle from './components/ColorModeToggle';
 import Signin from "./routes/Signin";
 import Home from './routes/Home';
+import Signup from './routes/Signup'
 
 const App = () => {
 
@@ -14,6 +15,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Signin/>}/>
           <Route path="/home" element={<Home/>}/>
+          <Route path="/signup" element={<Signup/>}/>
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
